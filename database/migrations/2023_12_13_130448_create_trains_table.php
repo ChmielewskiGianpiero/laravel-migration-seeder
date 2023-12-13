e9268e41f93e4c820a8f88c6c4b40d8785b860f9
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-12-13 17:54:14 +0100

    ADD - index, foreach trains

--- database/migrations/2023_12_13_130448_create_trains_table.php
+++ database/migrations/2023_12_13_130448_create_trains_table.php
@@ -14,15 +14,15 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('Azienda', 20);
-            $table->string('Stazione di partenza', 20);
-            $table->string('Stazione di arrivo', 20);
-            $table->time('Orario di partenza');
-            $table->time('Orario di arrivo');
-            $table->unsignedMediumInteger('Codice treno');
-            $table->unsignedTinyInteger('Numero carrozze');
-            $table->unsignedTinyInteger('In orario')->default(1);
-            $table->unsignedTinyInteger('Cancellato')->default(0);
+            $table->string('azienda', 20);
+            $table->string('stazione_di_partenza', 20);
+            $table->string('stazione_di_arrivo', 20);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->unsignedMediumInteger('codice_treno');
+            $table->unsignedTinyInteger('numero_carrozze');
+            $table->unsignedTinyInteger('in_orario')->default(1);
+            $table->unsignedTinyInteger('cancellato')->default(0);
         });
     }
 
--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -2,8 +2,17 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <h1 class="text-center">homepage</h1>
-        </div>
+        <div class="card-group">
+             @foreach ($trains as $train)
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $train->stazione_di_partenza}}</h5> 
+                        <p class="card-text">{{ $train->orario_di_partenza }}</p>
+                        <h6 class="card-title">{{ $train->stazione_di_arrivo}}</h6>
+                        <p class="card-text">{{ $train->orario_di_arrivo }}</p>
+                     </div>
+                </div>
+            @endforeach    
+            </div>
     </div>
 @endsection
