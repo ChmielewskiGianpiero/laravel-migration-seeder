6b8af4f5cc2c90b3a92e638e66cfddc5ba843b1c
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-12-13 18:06:26 +0100

    ADD - row table trains, style

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -2,13 +2,14 @@
 
 @section('content')
     <div class="container">
-        <div class="card-group">
+        <h1 class="text-center py-4">Treni in arrivo</h1>
+        <div class="card-group gap-2">
              @foreach ($trains as $train)
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">{{ $train->stazione_di_partenza}}</h5> 
+                        <h5 class="card-title">da {{ $train->stazione_di_partenza}}</h5> 
                         <p class="card-text">{{ $train->orario_di_partenza }}</p>
-                        <h6 class="card-title">{{ $train->stazione_di_arrivo}}</h6>
+                        <h6 class="card-title">a {{ $train->stazione_di_arrivo}}</h6>
                         <p class="card-text">{{ $train->orario_di_arrivo }}</p>
                      </div>
                 </div>
